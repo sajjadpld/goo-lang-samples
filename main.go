474ab9e985c4654b77a6d1276767731b0c505a59
Sajjad Pouladvand <51276430+sajjadpld@users.noreply.github.com>
2022-10-30 01:10:30 +0330

    Add files via upload

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+
+	var name = "sajjad"
+	family := "PLD"
+
+	fmt.Println(name, " ", family)
+
+	const age = 24
+	fmt.Println(age)
+
+	fmt.Printf("Hi, my name is %v and my last name is %v, im %v years old \n", name, family, age)
+
+	var fatherName string
+
+	fatherName = "Mahmoud"
+
+	fmt.Println(fatherName)
+
+	fmt.Printf("the type of fatherName is %T\n", fatherName)
+}
