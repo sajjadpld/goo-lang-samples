5f664826c4308069cb8fab210cfb36f6feb0180c
sajjadpld <sajjadpld@gmail.com>
2022-10-31 14:07:22 +0330

    until if ... else

--- main.go
+++ main.go
@@ -1,34 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello world")
+	str := "my name is sajjad"
 
-	var name = "sajjad"
-	family := "PLD"
-
-	fmt.Println(name, " ", family)
-
-	const age = 24
-	fmt.Println(age)
-
-	fmt.Printf("Hi, my name is %v and my last name is %v, im %v years old \n", name, family, age)
-
-	var fatherName string
-
-	fatherName = "Mahmoud"
-
-	fmt.Println(fatherName)
-
-	fmt.Println(&fatherName)
-
-	fmt.Printf("the type of fatherName is %T\n", fatherName)
-
-
-	var gfName string
-	fmt.Scan(&gfName)
-
-	fmt.Printf("your gir friend name is %v", gfName)
-
+	fmt.Println(strings.Fields(str))
 }
